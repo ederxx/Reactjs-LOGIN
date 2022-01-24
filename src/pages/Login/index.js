41723c18220e96163f2a2b77edf32c8012e073ce
ederxx <93275824+ederxx@users.noreply.github.com>
2022-01-24 12:02:49 -0300

    Finalizar tela de llogn

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,31 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './login.css'
-import { MdEmail } from "react-icons/md";
-import { MdOutlineLock } from "react-icons/md";
+import { MdEmail } from "react-icons/md"
+import { MdOutlineLock } from "react-icons/md"
+import { HiEye,HiEyeOff } from "react-icons/hi"
 function Login(){
-
+    const [email, setEmail] = useState("")
+    const [password, setPassword]=useState("")
+    const [show, setShow]=useState(false)
+    const handleClick = (e) =>{
+        e.preventDefault()
+        setShow(!show);
+    } 
     return (
         <div className="login">
        <div className="login-logo">
            <img src="https://flyclipart.com/downloadpage/images/user-logn-login-member-icon-with-png-and-vector-format-935695.png/935695" alt="Login App"/>
        </div>
        <div className="login-right">
-        <h1>Acessar App</h1>
+        <h1>LOGIN</h1>
 
         <div className="login-InputEmail">
-        <MdEmail/>
+        <MdEmail />
 
                 <input 
-                type="text"
-                placeholder="Digite um Email"></input>
-       <div className="login-InputPassword"></div>
+                type="email"
+                placeholder="Digite um Email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+
+                />
+                </div>
+       <div className="login-InputPassword">
        <MdOutlineLock/>
                
                      <input 
-                type="text"
-                placeholder="Digite sua senha"></input>
+                placeholder="Digite sua senha"
+                type={show ? "text" : "password"}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                />
               
+        <div className='login-eye'>
+        {show ? (
+            <HiEye size={20} 
+            onClick={handleClick}
+            />
+        ) : (
+            <HiEyeOff size={20}
+            onClick={handleClick}
+             />
+        )}
 
+        </div>
 
 
         </div>
